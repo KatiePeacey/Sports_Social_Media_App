3abb09046a4d47377bd05bece92eee54f252b2c4
KatiePeacey <katiepeacey@outlook.com>
2024-10-14 23:21:43 +0100

    Adding user page with optional id

--- routes/web.php
+++ routes/web.php
@@ -5,10 +5,21 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('/homepage', function () {
     return 'Hello, welcome to my first route :)';
 });
+
 Route::get('/anotherHome', function () {
     return 'This is another homepage.';
 });
+
 Route::redirect('/anotherHome', '/homepage', 301);
+
+Route::get('users/{id?}', function ($id = 'John Doe') {
+    return 'User page '.$id;
+});
+
+Route::get('users/{id}/comment/{commentId}', function ($id, $commentId) {
+    return 'User page '.$id. ' comment id '.$commentId;
+});
