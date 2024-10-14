b9f9af533b42eb0521778cc5562c294687d6f471
KatiePeacey <katiepeacey@outlook.com>
2024-10-14 23:11:00 +0100

    Adding in a redirect

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,7 @@ Route::get('/', function () {
 Route::get('/homepage', function () {
     return 'Hello, welcome to my first route :)';
 });
+Route::get('/anotherHome', function () {
+    return 'This is another homepage.';
+});
+Route::redirect('/anotherHome', '/homepage', 301);
