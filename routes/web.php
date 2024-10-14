dd7646eb0d9153e18f11cd6aa1474e93cb53708a
KatiePeacey <katiepeacey@outlook.com>
2024-10-14 23:04:06 +0100

    Very first route (homepage)

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,6 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::get('/homepage', function () {
+    return 'Hello, welcome to my first route :)';
+});
