f8d3f53eec4d3773d78e5809c0671c2421b50881
KatiePeacey <katiepeacey@outlook.com>
2024-10-31 19:20:06 +0000

    Creating new clubs model

---
+++ database/factories/ClubFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Club>
+ */
+class ClubFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            //
+            'name' => fake()->city(),
+            'teams' => fake()->numberBetween(1, 10),
+            'members' => fake()->numberBetween(1, 250),
+        ];
+    }
+}
---
+++ database/seeders/ClubTableSeeder.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use App\Models\Club;
+
+class ClubTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        $c = new Club;
+        $c->name = 'Gowerton';
+        $c->teams = 2;
+        $c->members = 150;
+        $c->save();
+
+        Club::factory()->count(50)->create();
+    }
+}
