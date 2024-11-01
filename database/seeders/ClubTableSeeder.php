84789d0e36f61615e14ef9ee4a8004c6cccd56f4
KatiePeacey <katiepeacey@outlook.com>
2024-11-01 21:16:13 +0000

    Adding comments to clubSeeder

--- database/seeders/ClubTableSeeder.php
+++ database/seeders/ClubTableSeeder.php
@@ -20,11 +20,9 @@ class ClubTableSeeder extends Seeder
         $c->members = 150;
         $c->save();
 
-       // Club::factory()->count(10)->create();
-
         Club::factory()
-        ->has(Umpire::factory()->count(3)) // Creates and links 3 umpires per club
-        ->count(5) // Creates 5 clubs
+        ->has(Umpire::factory()->count(3)) //Links 3 umpires for each club
+        ->count(10) //Creates 10 clubs
         ->create();
 }
 }
