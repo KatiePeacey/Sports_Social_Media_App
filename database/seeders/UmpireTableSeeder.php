d917eac06bbcbec8735992f5715b7bda09cdd342
KatiePeacey <katiepeacey@outlook.com>
2024-11-01 15:25:44 +0000

    Adding more umpires in factory

--- database/seeders/UmpireTableSeeder.php
+++ database/seeders/UmpireTableSeeder.php
@@ -24,7 +24,7 @@ class UmpireTableSeeder extends Seeder
         $u->clubs()->attach(1);
         $u->clubs()->attach(9);
 
-        Umpire::factory()->count(10)->create();
+        Umpire::factory()->count(20)->create();
 
     }
 }
