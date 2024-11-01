d5113a465c2ecb6d33872ebf5b0b466605ac1361
KatiePeacey <katiepeacey@outlook.com>
2024-11-01 22:03:26 +0000

    More coding conventions

--- database/factories/ClubFactory.php
+++ database/factories/ClubFactory.php
@@ -6,6 +6,7 @@ use Illuminate\Database\Eloquent\Factories\Factory;
 use App\Models\Club;
 use App\Models\Player;
 
+
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Club>
  */
