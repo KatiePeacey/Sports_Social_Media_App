c2c812fb81c0aa0b7a072825b45a233363568987
KatiePeacey <katiepeacey@outlook.com>
2024-11-13 14:32:15 +0000

    Adding authentication

--- routes/web.php
+++ routes/web.php
@@ -1,25 +1,20 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/homepage', function () {
-    return 'Hello, welcome to my first route :)';
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
-Route::get('/anotherHome', function () {
-    return 'This is another homepage.';
-});
-
-Route::redirect('/anotherHome', '/homepage', 301);
-
-Route::get('users/{id?}', function ($id = 'John Doe') {
-    return 'User page '.$id;
-});
-
-Route::get('users/{id}/comment/{commentId}', function ($id, $commentId) {
-    return 'User page '.$id. ' comment id '.$commentId;
-});
+require __DIR__.'/auth.php';
